cbcd2ee7fb16d5e160b515a44c8186d425a8fd9b
Cipta <ordinarypeople67@gmail.com>
2025-01-05 21:36:24 +0700

    fix: Typo Route

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forgot-Password" element={<ForgotPassword />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/DescCourse" element={<DescCourse />} />
       </Routes>
     </Router>
