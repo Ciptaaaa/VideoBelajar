aada468e3440aecfaa92d8b842a47503543c64a7
Cipta <ordinarypeople67@gmail.com>
2025-02-05 14:22:50 +0700

    Feat: Feat UseLocation for maping item

--- src/components/AdminHeader.jsx
+++ src/components/AdminHeader.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import NavbarLogo from "../assets/logo.png"
+import { Link, useLocation } from "react-router-dom";
+import NavbarLogo from "../assets/logo.png";
 
 const AdminHeader = () => {
+  const location = useLocation();
   return (
     <aside className="w-full md:w-64 bg-white shadow-md p-4 md:fixed md:h-full md:overflow-y-auto">
       <div className="flex items-center justify-center mb-8">
@@ -16,30 +17,20 @@ const AdminHeader = () => {
       </div>
       <nav>
         <ul>
-          <li className="mb-4">
-            <Link
-              className="block text-gray-700 hover:bg-gray-200 rounded-md p-3 bg-gray-200"
-              to="/Admin/Dashboard"
-            >
-              Dashboard
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              className="block text-gray-700 hover:bg-gray-200 rounded-md p-3 "
-              to="/Admin/Product"
-            >
-              Product
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              className="block text-gray-700 hover:bg-gray-200 rounded-md p-3 "
-              to="/Admin/AddUser"
-            >
-              Add User
-            </Link>
-          </li>
+          {[
+            { name: "Dashboard", path: "/Admin/Dashboard" },
+            { name: "Add Product", path: "/Admin/Product" },
+            { name: "Add User", path: "/Admin/AddUser" },
+          ].map((item) => (
+            <li className="mb-4" key={item.path}>
+              <Link
+                className={`block text-gray-700 hover:bg-gray-200 rounded-md p-3 ${
+                  location.pathname === item.path ? "bg-gray-300" : "" }`}
+                to={item.path}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
